5b7b6ac8b3dcd6beea0b7fa0121053cb51627747
Michel Catudal <michelcatudal@gmail.com>
2022-01-22 22:03:19 -0500

    saved changes

--- IAR/settings/RL78_G14.Debug.cspy.bat
+++ IAR/settings/RL78_G14.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\Users\miche\Documents\RL78_G14_32pins\settings\RL78_G14.Debug.general.xcl" --backend -f "C:\Users\miche\Documents\RL78_G14_32pins\settings\RL78_G14.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\Users\miche\Documents\RL78_G14_32p\IAR\settings\RL78_G14.Debug.general.xcl" --backend -f "C:\Users\miche\Documents\RL78_G14_32p\IAR\settings\RL78_G14.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\Users\miche\Documents\RL78_G14_32pins\settings\RL78_G14.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\miche\Documents\RL78_G14_32pins\settings\RL78_G14.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\Users\miche\Documents\RL78_G14_32p\IAR\settings\RL78_G14.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\miche\Documents\RL78_G14_32p\IAR\settings\RL78_G14.Debug.driver.xcl" 
 
 @echo off 
 :end
--- IAR/settings/RL78_G14.Debug.cspy.ps1
+++ IAR/settings/RL78_G14.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\Users\miche\Documents\RL78_G14_32pins\settings\RL78_G14.Debug.general.xcl" --backend -f "C:\Users\miche\Documents\RL78_G14_32pins\settings\RL78_G14.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\Users\miche\Documents\RL78_G14_32p\IAR\settings\RL78_G14.Debug.general.xcl" --backend -f "C:\Users\miche\Documents\RL78_G14_32p\IAR\settings\RL78_G14.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\Users\miche\Documents\RL78_G14_32pins\settings\RL78_G14.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\miche\Documents\RL78_G14_32pins\settings\RL78_G14.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\Users\miche\Documents\RL78_G14_32p\IAR\settings\RL78_G14.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\miche\Documents\RL78_G14_32p\IAR\settings\RL78_G14.Debug.driver.xcl" 
 }
