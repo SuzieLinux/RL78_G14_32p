ce6d139aa01c240398090e8e121e0034c1d3279d
Michel Catudal <michelcatudal@gmail.com>
2022-02-05 20:49:42 -0500

    ..

--- IAR/src/base/r_cg_userdefine.h
+++ IAR/src/base/r_cg_userdefine.h
@@ -2,15 +2,15 @@
 * DISCLAIMER
 * This software is supplied by Renesas Electronics Corporation and is only intended for use with Renesas products.
 * No other uses are authorized. This software is owned by Renesas Electronics Corporation and is protected under all
-* applicable laws, including copyright laws. 
+* applicable laws, including copyright laws.
 * THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING THIS SOFTWARE, WHETHER EXPRESS, IMPLIED
 * OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 * NON-INFRINGEMENT.  ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY
 * LAW, NEITHER RENESAS ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE FOR ANY DIRECT,
 * INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR
 * ITS AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-* Renesas reserves the right, without notice, to make changes to this software and to discontinue the availability 
-* of this software. By using this software, you agree to the additional terms and conditions found by accessing the 
+* Renesas reserves the right, without notice, to make changes to this software and to discontinue the availability
+* of this software. By using this software, you agree to the additional terms and conditions found by accessing the
 * following link:
 * http://www.renesas.com/disclaimer
 *
@@ -33,6 +33,17 @@
 User definitions
 ***********************************************************************************************************************/
 
-/* Start user code for function. Do not edit comment generated here */
-/* End user code. Do not edit comment generated here */
+/* Defines the maximum number of writes the EEPROM supports per page write */
+#define EEPROM_MAX_WRITE        8u
+/* Defines the number of expected bytes to be received from the slave */
+#define EEPROM_RECEIVE_COUNT        9u
+
+/* Defines the address the EEPROM device responds to on the IIC bus */
+/* A0 = 160 decimal */
+#define EEPROM_DEVICE_ADDRESS       0xA0
+
+/* Define the number of bytes used for addressing the EEPROM's memory */
+#define EEPROM_WORD_ADDRESS     2u
+/* Defines the total number of data to be sent to the slave */
+#define EEPROM_NUMB_WRITE       11u
 #endif
