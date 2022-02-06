36527f848aa66cf9588989692058ca96fcb2d27b
Michel Catudal <michelcatudal@gmail.com>
2022-02-05 20:44:53 -0500

    ..

--- IAR/src/base/r_systeminit.c
+++ IAR/src/base/r_systeminit.c
@@ -2,15 +2,15 @@
 * DISCLAIMER
 * This software is supplied by Renesas Electronics Corporation and is only intended for use with Renesas products.
 * No other uses are authorized. This software is owned by Renesas Electronics Corporation and is protected under all
-* applicable laws, including copyright laws. 
+* applicable laws, including copyright laws.
 * THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING THIS SOFTWARE, WHETHER EXPRESS, IMPLIED
 * OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 * NON-INFRINGEMENT.  ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY
 * LAW, NEITHER RENESAS ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE FOR ANY DIRECT,
 * INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR
 * ITS AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-* Renesas reserves the right, without notice, to make changes to this software and to discontinue the availability 
-* of this software. By using this software, you agree to the additional terms and conditions found by accessing the 
+* Renesas reserves the right, without notice, to make changes to this software and to discontinue the availability
+* of this software. By using this software, you agree to the additional terms and conditions found by accessing the
 * following link:
 * http://www.renesas.com/disclaimer
 *
@@ -60,7 +60,7 @@ void R_Systeminit(void);
 void R_Systeminit(void)
 {
     volatile uint32_t w_count;
-    
+
     PIOR0 = 0x00U;
     PIOR1 = 0x00U;
     R_CGC_Get_ResetSource();
@@ -69,7 +69,6 @@ void R_Systeminit(void)
     R_SAU0_Create();
     R_IICA0_Create();
     R_TAU0_Create();
-    R_RTC_Create();
     R_INTC_Create();
     R_TMR_RD0_Create();
     IAWCTL = 0x00U;
